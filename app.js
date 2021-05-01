60dac89b3116df17677a443faa30fcb00c4f5031
firecoder1 <55807755+firecoder1@users.noreply.github.com>
2021-05-01 17:36:03 +0100

    Create app.js

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+const { project_id, project_creator } = require("settings.json");
+
 const cluster = require("cluster");
 
 if (cluster.isMaster) {
@@ -18,10 +20,15 @@ function managerProcess() {
 
 function workerProcess() {
   // This is the worker!
-  const phin = require("phin")
-  
+  const phin = require("phin");
+
   setInterval(60000, async () => {
     try {
-      const res = await pg
-  })
+      const res = await phin(
+        `https://api.scratch.mit.edu/users/${project_creator}/projects/${project_id}/comments`
+      );
+      if (res.statusCode == 200) {
+        
+    } catch {}
+  });
 }
